b63517d29d87f5d64208f1c74d1a4316dd19cc40
D6-80621-Kushagra-Khatri <144333385+kushagrakhatri@users.noreply.github.com>
2025-04-04 18:45:11 +1100

    Cleaned up the logic, validating the input and added comments

--- main.rb
+++ main.rb
@@ -1,27 +1,35 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
-puts "Please enter the number:"
-num = gets
-len = num.length
-num = num.reverse!.to_i
-sum  = 0 
-ans = ''
-count = 0
-len.times do
-    digit = num % 10
-    ans += digit.to_s 
-    num = num / 10
-    if count % 2 == 0
-        sum += digit
-    else
-        sum += (digit * 3)
-    end
-    if num == 0 
-        break
-    end
-    count += 1
+puts "Please enter the first 12 digits of an ISBN:"
+
+#get the input; remove traliing whitespace
+incomplete_ISBN = gets.chomp  
+
+#validating that the input is of 12 digits
+unless /^[\d]{12}$/.match(incomplete_ISBN)
+    puts "Not a valid input (should be 12 digits)"
+    exit
 end
-last_digit = 10 - (sum % 10)
-ans = ans + last_digit.to_s
+sum  = 0 
 
-puts ans
+
+#itearting over each digit; alternatively add the digit or multiplying by 3 and then adding it to the sum
+incomplete_ISBN.each_char.with_index do |digit, i|
+
+  if i.even?
+    sum += digit.to_i
+  else
+    sum += (digit.to_i * 3)
+  end
+end
+
+#alterantive way to get the sum!
+sum2 = incomplete_ISBN.each_char.with_index.map { |n, i| i.even? ? n.to_i : (n.to_i * 3)}.sum
+
+#getting the last digit by modding the sum by 10 then substracting it by 10 (also 0 in case of sum is 0)
+last_digit = (10 - (sum % 10)) % 10
+
+#appending the new digit calculated to orginal
+complete_ISBN = incomplete_ISBN + last_digit.to_s
+
+puts "Complete ISBN is : " + complete_ISBN
