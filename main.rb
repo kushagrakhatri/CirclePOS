a06079f7331f81b56e34de08bcc996c2ad07e426
D6-80621-Kushagra-Khatri <144333385+kushagrakhatri@users.noreply.github.com>
2025-04-04 16:22:07 +1100

    Naive solution

--- main.rb
+++ main.rb
@@ -5,12 +5,23 @@ num = gets
 len = num.length
 num = num.reverse!.to_i
 sum  = 0 
+ans = ''
+count = 0
 len.times do
-    puts num % 10
+    digit = num % 10
+    ans += digit.to_s 
     num = num / 10
-
-
+    if count % 2 == 0
+        sum += digit
+    else
+        sum += (digit * 3)
+    end
     if num == 0 
         break
     end
+    count += 1
 end
+last_digit = 10 - (sum % 10)
+ans = ans + last_digit.to_s
+
+puts ans
